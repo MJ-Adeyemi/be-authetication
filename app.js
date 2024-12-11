901432f91cb938891625843b00f0010ca4541f81
MAJEED A. ADEYEMI <adeyemimajeedabolayo92@gmail.com>
2024-12-11 13:21:07 +0100

    1112

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ const express = require("express");
 const dotenv = require("dotenv")
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
+const cors = require("cors");
 const router = require("./routes/user")
 dotenv.config();
 const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT;
 
+
+
+
+// let corsOptions = {
+//   origin: ["http://localhost:5500"]
+// };
+
+// app.use(cors(corsOptions));
 connectDB();
 app.use(bodyParser.json());
 app.use('/api', router)
 
 app.listen(PORT, () =>{
-    console.log("server running ")
+    console.log("server running")
 })
